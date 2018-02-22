b01ec8a9f5f6b3cd5a664c2e87b724a0034f9b61
KuzmenkoLiz <lizaveta330@gmail.com>
2018-02-22 23:22:18 +0300

    'result'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,52 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.compareFunction = function(a, b) {return a-b};
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    function compare(a, b) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+    }
+    indices.sort(compare);
+    var mass = [];
+
+    for (var i = 0; i < indices.length; i++) {
+      mass.push(this.array[indices[i]]);
+    }
+    mass.sort(this.compareFunction);
+
+    for (var i = 0; i < indices.length; i++){
+      this.array.splice(indices[i], 1, mass[i]);
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
